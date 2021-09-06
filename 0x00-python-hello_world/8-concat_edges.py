9835f0b5b5bd18f60916bfd3dd546dc4f30ad949
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-09-06 12:38:54 +0000

    Printing string in Python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[39:67] + str[107:112] + str[:6]
-print(str)
+print(str) 
