ae8daa24c85e32ab3e3bc21f682965aceed38fe5
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-09-27 21:58:51 +0300

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    return 98 + a ** b
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return (a * b) - c
