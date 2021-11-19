14ebf8bc1ecb3c4a566d09e48859ccd8a00f3cb7
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-11-19 06:06:39 +0300

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,13 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
+-- Create database 'hbtn_0d_2' and user 'user_0d_2'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+/* Create user 'user_0d_2' */
+CREATE USER IF NOT EXISTS user_0d_2@localhost;
+
+/* Set new user's password */
+SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';
+
+/* Grant new user SELECT privilege */
 GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+	ON hbtn_0d_2.*
+	TO user_0d_2@localhost;
