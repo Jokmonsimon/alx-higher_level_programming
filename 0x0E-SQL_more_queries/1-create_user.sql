6751b2d098577d19944e58ea102696f939c49308
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-11-19 06:07:57 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,10 @@
--- Creates the user user_0d_2 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
+-- Create a MySQL server user 'user_0d_1' whose password is 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS user_0d_1@localhost;
+
+/* SET password for new user */
+SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
+
+/* Grant new user all privileges on the MySQL Server */
 GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+	ON *.*
+	TO user_0d_1@localhost;
