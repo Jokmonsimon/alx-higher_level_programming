52b6dd19064adb446f2c961c1690873ffca6bc62
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-11-19 05:42:43 +0300

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,19 @@
 -- Convert database to utf8
-USE hbtn_0c_0;
--- Convert database to utf8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Convert table to utf8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Convert name to utf8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE
+	hbtn_0c_0
+	CHARACTER SET = utf8mb4
+	COLLATE = utf8mb4_unicode_ci;
+
+-- convert table "first_table' to UTF8
+ALTER TABLE
+	hbtn_0c_0.first_table
+      CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
+
+-- convert column 'name' to UTF8
+ALTER TABLE
+	hbtn_0c_0.first_table
+	MODIFY name
+	VARCHAR(256)
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
