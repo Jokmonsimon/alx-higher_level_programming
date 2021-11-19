b98d75c905f5b9d84be5d0d42281cb9fbe862e25
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-11-18 18:38:50 -0800

    Convert database, table and column to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Convert database to utf8
+USE hbtn_0c_0;
+-- Convert database to utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert table to utf8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert name to utf8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
