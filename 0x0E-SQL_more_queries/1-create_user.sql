41f115f7d91494cfa9ae1440405a6c8ce8380e70
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-11-19 05:58:28 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
--- Creates the user user_0d_1 with all privileges.
+-- Creates the user user_0d_2 with all privileges.
 CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
 GRANT ALL PRIVILEGES
    ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
