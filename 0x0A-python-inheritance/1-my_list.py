08171bfa9bbb47f9a86902b597a69166a7edfe35
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-11-19 06:32:00 +0300

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,6 @@
 # Brennan D Baraban <375@holbertonschool.com>
 """Defines an inherited list class MyList."""
 
-
 class MyList(list):
     """Implements sorted printing for the built-in list class."""
 
