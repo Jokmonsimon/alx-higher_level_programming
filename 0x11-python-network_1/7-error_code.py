9fb396b42c456bd0f2de425bb5fed4fc949d4d67
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-06-05 16:59:10 +0300

    Hot fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Response header value"""
-import requests
-from requests.exceptions import HTTPError
-import sys
+"""
+same as 3-error_code with requests model
+"""
 
-if __name__ == "__main__":
-    req = requests.get(sys.argv[1])
-    if req.status_code >= 400:
-        print("Error code: {}".format(req.status_code))
+if __name__ == '__main__':
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(req.text)
+        print(r.text)
