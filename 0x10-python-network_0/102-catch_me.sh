169a81de53b3a3e7354195368e633f79a5a95f47
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-06-05 16:34:38 +0300

    New update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "You got me!" -d "user_id=98"
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
